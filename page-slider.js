e7ce28613903f642c69520fb024373b96220f408
fadingbeat <fadingbeat@gmail.com>
2021-03-08 08:52:47 +0100

    change to ES6 syntax

--- page-slider.js
+++ page-slider.js
@@ -1,27 +1,27 @@
-$(document).ready(function () {
-    var show = 8;
+$(document).ready( () => {
+    let show = 8;
     //calculate each slides width depending on how many you want to show
-    var w = $('.wrapper').width() / show;
+    let w = $('.wrapper').width() / show;
     
-    function slider() {
-        $(".btn_left").click(function() {
+    () => {
+        $(".btn_left").click( () => {
             $('.item:first-child').animate({
                 marginLeft: -w,
                 opacity: '0.4'
-            }, 'slow', function () {
+            }, 'slow', () => {
                 $(this).appendTo($(this).parent()).css({marginLeft: 0, opacity: 1});
             });
         });
 
-        $(".btn_right").click(function() {
+        $(".btn_right").click( () => {
             $('.item:first-child').animate({
                 marginRight: -w,
                 opacity: '0.4'
-            }, 'slow', function () {
+            }, 'slow', () => {
                 $(this).appendTo($(this).parent()).css({marginRight: 0, opacity: 1});
             });
         });
     }
-    var timer;
+    const timer;
     timer = setInterval(slider, 1000);
 });
