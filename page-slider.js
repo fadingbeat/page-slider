0e331b9854306f3cf336526fd2491efd071bfbd3
fadingbeat <fadingbeat@gmail.com>
2021-03-16 10:22:30 +0100

    rewrite js using arrows functions

--- page-slider.js
+++ page-slider.js
@@ -1,50 +1,31 @@
-// var first = $('.item').eq(0);
-// var count = $('.item').length;
-// var last = $('.item').eq(count - 1);
-// first.addClass('active');
-var w = $('.slider').length;
+var nextSlide = (target) => {
+  element_clone = $('.item').eq(target).clone();
+  $('.item').eq(target).remove();
+  $('.slider').append(element_clone);
+};
 
-
-$('.btn_right').click(function() {
+$('.btn_right').click( () => {
   var li = $('li.item.col.active');
-  var slide = function() {
+  var slide = () => {
     target = $('.active').index();
     nextSlide(target);
+    if (li.length)
+      li.removeClass("active").next().addClass("active");
+    else
+      jQuery("li").first().addClass("active");
   }
-    
-  var nextSlide = function(target) {
-    element_clone = $('.item').eq(target).clone();
-    $('.item').eq(target).remove();
-    $('.slider').append(element_clone);
-  };
-    
   slide();
-
-  if (li.length)
-    li.removeClass("active").next().addClass("active");
-  else
-    jQuery("li").first().addClass("active");
 });
 
-$('.btn_left').click(function() {
+$('.btn_left').click( () => {
   var li = $('li.item.col.active');
-  var slide = function() {
+  var slide = () => {
     target = $('.active').index();
     nextSlide(target);
-
     if (li.length)
       li.removeClass("active").prev().addClass("active");
     else
       jQuery("li").last().addClass("active");
   }
-    
-  var nextSlide = function(target) {
-    element_clone = $('.item').eq(target).clone();
-    $('.item').eq(target).remove();
-    $('.slider').append(element_clone);
-  };
-    
   slide();
-
-  
 });
