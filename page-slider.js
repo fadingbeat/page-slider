6574aa45cbd11fec2200e3a93103efc50c3b3eec
fadingbeat <fadingbeat@gmail.com>
2021-03-16 10:10:17 +0100

    js different approach

--- page-slider.js
+++ page-slider.js
@@ -1,23 +1,50 @@
-let show = 9;
-let w = $('.slider').width() / show;
-var first = $('.item').eq(0);
-var count = $('.item').length;
-var last = $('.item').eq(count - 1);
-first.addClass('active');
+// var first = $('.item').eq(0);
+// var count = $('.item').length;
+// var last = $('.item').eq(count - 1);
+// first.addClass('active');
+var w = $('.slider').length;
+
 
 $('.btn_right').click(function() {
+  var li = $('li.item.col.active');
   var slide = function() {
     target = $('.active').index();
     nextSlide(target);
   }
-
+    
   var nextSlide = function(target) {
-    $('.item').removeClass('active').eq(target+1).addClass('active');
     element_clone = $('.item').eq(target).clone();
     $('.item').eq(target).remove();
     $('.slider').append(element_clone);
   };
-
+    
   slide();
 
+  if (li.length)
+    li.removeClass("active").next().addClass("active");
+  else
+    jQuery("li").first().addClass("active");
+});
+
+$('.btn_left').click(function() {
+  var li = $('li.item.col.active');
+  var slide = function() {
+    target = $('.active').index();
+    nextSlide(target);
+
+    if (li.length)
+      li.removeClass("active").prev().addClass("active");
+    else
+      jQuery("li").last().addClass("active");
+  }
+    
+  var nextSlide = function(target) {
+    element_clone = $('.item').eq(target).clone();
+    $('.item').eq(target).remove();
+    $('.slider').append(element_clone);
+  };
+    
+  slide();
+
+  
 });
